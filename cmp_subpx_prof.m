416ee24dab3b5b4c5b627ecef9c02d136b9a2923
Seong Dae Yun <108725879+SeongDaeYun@users.noreply.github.com>
2026-06-10 09:08:18 +0200

    Update cmp_subpx_prof.m

--- cmp_subpx_prof.m
+++ cmp_subpx_prof.m
@@ -21,7 +21,10 @@
 %           hi           | figure handle
 %           --------------------------------------------------------------
 %
-% * dev.: Seong Dae Yun (s.yun@fz-juelich.de)
+% * Dev.: Seong Dae Yun (s.yun@fz-juelich.de)
+% * Ref.: Yun et al. A Novel Distortion-Matched Anatomical Imaging Sequence for 
+%         High-Fidelity Functional Mapping in Submillimeter-Resolution fMRI.
+%         Sci Rep. 2026. (under revision)
 % ========================================================================
 
 
